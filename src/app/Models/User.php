ae594cef3e59539e984ab838b998bd905a4c8d8f
mariko inukai <mariko.inukai1006@gmail.com>
2025-12-20 14:53:24 +0900

    contact form related files

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -2,20 +2,28 @@
 
 namespace App\Models;
 
-// 修正ポイント1: 継承元を Authenticatable に変更するための use 宣言
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-// use Illuminate\Database\Eloquent\Model;
-
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
-    
-        protected $fillable = [
-           'name',
-           'email',
-           'password'
-        ];
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 }
